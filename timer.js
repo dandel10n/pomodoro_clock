404ef7049678e698aea6d8b96d027482c17dc334
Mariya Radchuk <radchuk.mary@gmail.com>
2017-03-10 18:34:07 +0300

    Initial commit

---
+++ timer.js
@@ -0,0 +1,34 @@
+var pomodoroClock = function() {
+  var sessionLength = 25;
+  var breakLength = 5;
+
+
+  this.startTimer = function() {};
+  this.stopTimer = function() {};
+  this.sessionMinutePlus = function() {
+    sessionLength += 1;
+  };
+  
+  this.sessionMinuteMinus = function() {
+    sessionLength -= 1;
+  };
+  
+  this.breakMinutePlus = function() {
+    breakLength += 1;
+  };
+  this.breakMinuteMinus = function() {
+    breakLength -= 1;
+  };
+  
+  this.getSessionTime = function() {
+    return sessionLength;
+  };
+  
+  this.getBreakTime = function() {
+    return breakLength;
+  };
+  
+  //this.workTimeIsOver = function() {};
+  //this.breakTimeIsOver = function() {};
+  
+}
