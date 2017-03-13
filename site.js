1ee2fb8d99def0673896573d8e70dc471026d92a
Mariya Radchuk <radchuk.mary@gmail.com>
2017-03-13 13:50:01 +0300

    Make -/+ funcions work

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
+$(document).ready(function() {
+  var clock = new pomodoroClock();
 
+  $('#pomodoroClock .sessionMinus').click(function() {
+    clock.sessionMinuteMinus();
+    $(".sessionLength").text(clock.getSessionLength());
+  });
+
+  $('#pomodoroClock .sessionPlus').click(function() {
+    clock.sessionMinutePlus();
+    $(".sessionLength").text(clock.getSessionLength());
+  });
+
+  $('#pomodoroClock .breakMinus').click(function() {
+    clock.breakMinuteMinus();
+    $(".breakLength").text(clock.getBreakLength());
+  });
+
+  $('#pomodoroClock .breakPlus').click(function() {
+    clock.breakMinutePlus();
+    $(".breakLength").text(clock.getBreakLength());
+  });
+})
--- timer.js
+++ timer.js
@@ -2,33 +2,32 @@ var pomodoroClock = function() {
   var sessionLength = 25;
   var breakLength = 5;
 
-
   this.startTimer = function() {};
   this.stopTimer = function() {};
+
   this.sessionMinutePlus = function() {
     sessionLength += 1;
   };
-  
+
   this.sessionMinuteMinus = function() {
     sessionLength -= 1;
   };
-  
+
   this.breakMinutePlus = function() {
     breakLength += 1;
   };
   this.breakMinuteMinus = function() {
     breakLength -= 1;
   };
-  
-  this.getSessionTime = function() {
+
+  this.getSessionLength = function() {
     return sessionLength;
   };
-  
-  this.getBreakTime = function() {
+
+  this.getBreakLength = function() {
     return breakLength;
   };
-  
+
   //this.workTimeIsOver = function() {};
   //this.breakTimeIsOver = function() {};
-  
 }
